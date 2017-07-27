f1ab76213b844db0ef2e594ed7adf4cee6dc8b28
moocows111111 <30416286+moocows111111@users.noreply.github.com>
2017-07-27 15:01:33 -0400

    Crop.py

---
+++ Crop.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os
+import cv2
+import glob
+
+#I will comment these out.
+#directory = os.path.abspath('/home/moocows/PycharmProjects/Aim/data/')
+#file_paths = os.listdir(directory)
+images = glob.glob("*.jpg")
+
+for image in images:
+    img = cv2.imread(image,1)
+    path = '/home/moocows/PycharmProjects/Aim/croppeddata/'
+    crop_img = img[(540-200):(540+200), (960-200):(960+200)]
+
+    cv2.imwrite(str(path) + image, crop_img)
+
+    cv2.destroyAllWindows()
+
+
+
