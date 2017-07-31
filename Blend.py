3278866988756e441f7f82efd6749a27addf147c
moocows111111 <30416286+moocows111111@users.noreply.github.com>
2017-07-31 18:34:37 -0400

    Add files via upload
    Basic blending. Just need to chunk the directory and use images from newly created chunk

---
+++ Blend.py
@@ -0,0 +1,54 @@
+from PIL import Image
+
+#Chunk the directory
+#get chunks
+
+#basic operation...
+
+
+
+
+a= "frame144.jpg"
+b="frame145.jpg"
+c="frame146.jpg"
+d="frame147.jpg"
+e="frame148.jpg"
+f="frame149.jpg"
+g="frame150.jpg"
+h="frame151.jpg"
+i="frame152.jpg"
+j="frame153.jpg"
+k="frame154.jpg"
+
+img1 = Image.open(a)
+img2 = Image.open(b)
+img3 = Image.open(c)
+img4 = Image.open(d)
+img5 = Image.open(e)
+img6 = Image.open(f)
+img7 = Image.open(g)
+img8 = Image.open(h)
+img9 = Image.open(i)
+img10 = Image.open(j)
+img11 = Image.open(k)
+
+imgbld1 = Image.blend(img1, img2, 0.3)
+imgbld2 = Image.blend(imgbld1, img3, 0.3)
+imgbld3 = Image.blend(imgbld2, img4, 0.3)
+imgbld4 = Image.blend(imgbld3, img5, 0.3)
+imgbld5 = Image.blend(imgbld4, img6, 0.3)
+imgbld6 = Image.blend(imgbld5, img7, 0.3)
+imgbld7 = Image.blend(imgbld6, img8, 0.3)
+imgbld8 = Image.blend(imgbld7, img9, 0.3)
+imgbld9 = Image.blend(imgbld8, img10, 0.3)
+imgbld10 = Image.blend(imgbld9, img11, 0.3)
+
+imgbld10.show()
+
+
+
+
+
+
+
+
