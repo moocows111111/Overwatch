e1fbb14d0bff62a3881f0ec1165a35b3732f5ee0
moocows111111 <30416286+moocows111111@users.noreply.github.com>
2017-07-31 19:00:08 -0400

    Update Blend.py

--- Blend.py
+++ Blend.py
@@ -3,9 +3,24 @@ from PIL import Image
 n = 10
 
 #this will give me the chunks I want but I dont know how to access it and operate on one chunk at a time. I might have to do def chunks instead.
-for i in range(0 , len(data), n)
-  chunks = data[i:i+n]
-  
+#for i in range(0 , len(data), n)
+  #chunks = data[i:i+n]
+#Toy example of the idea thus far.
+data = [1,2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+print("this is the data ")
+print(data)
+
+
+def chunker(data,size):
+    for i in range(0, len(data), size):
+        yield data[i:i + size]
+
+# trying to show wtf is going on here
+print('what does it look like')
+
+for j in chunker(data, 4):
+    print(j)
   
 
 #Chunk the directory
